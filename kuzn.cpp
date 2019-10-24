0ec0ed158c0b27add198e5b26fc9667e8286240b
Demiurg-in-real <bezginandrew@gmail.com>
2019-10-24 02:44:52 -0400

    Clear comments

--- kuzn.cpp
+++ kuzn.cpp
@@ -1,6 +1,5 @@
 #include<iostream>
 #include<sys/types.h>
-#include<unistd.h>
 
 const static uint8_t Pi[256] = {252, 238, 221, 17, 207, 110, 49, 22, 251, 196, 250, 218, 35, 197, 4, 77, 233, 119, 240, 219, 147, 46, 153, 186, 23, 54, 241, 187, 20, 205, 95, 193, 249, 24, 101, 90, 226, 92, 239, 33, 129, 28, 60, 66, 139, 1, 142, 79, 5, 132, 2, 174, 227, 106, 143, 160, 6, 11, 237, 152, 127, 212, 211, 31, 235, 52, 44, 81, 234, 200, 72, 171, 242, 42, 104, 162, 253, 58, 206, 204, 181, 112, 14, 86, 8, 12, 118, 18, 191, 114, 19, 71, 156, 183, 93, 135, 21, 161, 150, 41, 16, 123, 154, 199, 243, 145, 120, 111, 157, 158, 178, 177, 50, 117, 25, 61, 255, 53, 138, 126, 109, 84, 198, 128, 195, 189, 13, 87, 223, 245, 36, 169, 62, 168, 67, 201, 215, 121, 214, 246, 124, 34, 185, 3, 224, 15, 236, 222, 122, 148, 176, 188, 220, 232, 40, 80, 78, 51, 10, 74, 167, 151, 96, 115, 30, 0, 98, 68, 26, 184, 56, 130, 100, 159, 38, 65, 173, 69, 70, 146, 39, 94, 85, 47, 140, 163, 165, 125, 105, 213, 149, 59, 7, 88, 179, 64, 134, 172, 29, 247, 48, 55, 107, 228, 136, 217, 231, 137, 225, 27, 131, 73, 76, 63, 248, 254, 141, 83, 170, 144, 202, 216, 133, 97, 32, 113, 103, 164, 45, 43, 9, 91, 203, 155, 37, 208, 190, 229, 108, 82, 89, 166, 116, 210, 230, 244, 180, 192, 209, 102, 175, 194, 57, 75, 99, 182};
 
@@ -31,10 +30,8 @@ static uint8_t polinMultiple[256][256];
 namespace blockKuz{
 
 	void generateTable(){
-		unsigned short int promez1=0x0, promez2=0x0;
-	       	unsigned short int smeshenie=0x1;
-		bool check=false;
-		int counter=0;
+		unsigned short int promez1, promez2, smeshenie;
+		bool check;
 		//Пробегаем по всем возможным вариантам перемножения полиномов
 		for(short int i=1;i<256;i++){
 			for(short int j=1;j<256;j++){
@@ -42,15 +39,12 @@ namespace blockKuz{
 				promez2=0x0;
 				smeshenie=0x1;	
 				check=false;
-				//std::cout<<counter<<"\n";
-				counter++;
 				//умножение полиномов
 			
 				for (int l=0;l<8;l++){
 					if((j>>l)&0x1 == 0x1){
 						promez1^=(i<<l);
 					}
-//					std::cout<<promez1;
 				}
 
 				//нахождение максимальной степени
@@ -70,26 +64,17 @@ namespace blockKuz{
 				//дальше надо реализовать второй блок с подбором целого для вычисления коэффициента (читать в тетради)
 
 				for(;;){
-					//sleep(2);
 					for (int l=0;l<8;l++){
-						//проверилstd::cout<<"Her\n";
 						if((smeshenie>>l)&0x1){
 							promez2^=(polynom<<l);
 						}
 					}
 
-
-					//std::cout<<"Nu nahui\n";
-
 					promez2^=promez1;
 
-					//std::cout<<"Vot suka!\n";
 					int c=15;
 					while(c>6){
-						//printf("%i\n",c);
-						//std::cout<<"Blya\n";
 						if( ((promez2>>c)&0x1) == 0x1 && (c>7)){
-							//std::cout<<"Hi\n";
 							smeshenie++;
 							promez2=0x0;
 							break;
@@ -101,7 +86,6 @@ namespace blockKuz{
 						}
 						c--;
 					}
-					//printf("%i\n%i %i\n",polinMultiple[i][j],i,j);
 					if (check) break;
 				}
 				printf("%i\n%i %i\n",polinMultiple[i][j],i,j);
@@ -137,7 +121,6 @@ namespace blockKuz{
 	void LSX::X(uint64_t *key, uint64_t *block){
 		block[0]^=key[0];
 		block[1]^=key[1];
-//		return (void*)block;
 	};
 
 	void LSX::S(uint64_t *block){
@@ -159,11 +142,11 @@ namespace blockKuz{
 };
 int main(){
 	blockKuz::generateTable();
-	for(int i=0;i<16;i++){
-		for(int j=0;j<16;j++){
-			printf("%i ", polinMultiple[i][j]);
-		}
-		printf("\n");
-	}
+//	for(int i=0;i<256;i++){
+//		for(int j=0;j<256;j++){
+//			printf("%i ", polinMultiple[i][j]);
+//		}
+//		printf("\n");
+//	}
 	return 0;
 }
